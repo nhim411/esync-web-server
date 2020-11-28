206cee854fa077c0a525fa09b15da9fe566d7689
Nam <lhnam411@gmail.com>
2020-11-28 16:53:58 +0700

    first commit

---
+++ AsyncTCP/libraries.h
@@ -0,0 +1,16 @@
+// Import required libraries
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <ESP32_FTPClient.h>
+#include "ESPAsyncWebServer.h"
+#include <AsyncTCP.h>
+#include <SPIFFS.h>
+#include "ESPmDNS.h"
+
+#include "lib/LiquidCrystal_PCF8574.h"
+#include "lib/Button.h"
+#include "Wire.h"
+#include "lib/RTClib.h"
+#include "SD.h"
+#include <ETH.h>
